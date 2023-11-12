ce8fbc44e2a7713fc13b4dfef426227d22407112
wda067 <mg6359@naver.com>
2023-11-12 19:21:47 +0900

    feat: 이벤트 배지 출력 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -51,4 +51,9 @@ public class OutputView {
         System.out.println("\n<할인 후 예상 결제 금액>");
         System.out.println(total - discount);
     }
+
+    public void printEventBadge(String badge) {
+        System.out.println("\n<12월 이벤트 배지>");
+        System.out.println(badge);
+    }
 }
