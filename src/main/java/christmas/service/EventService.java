83167b85225dc829a3e7f81dcad1f1c572f34d52
wda067 <mg6359@naver.com>
2023-11-12 19:14:50 +0900

    feat: 평일 디저트 메뉴 할인 기능

--- src/main/java/christmas/service/EventService.java
+++ src/main/java/christmas/service/EventService.java
@@ -1,8 +1,21 @@
 package christmas.service;
 
+import christmas.model.Menu;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+
 public class EventService {
 
     public int calculateXmasDiscount(int day) {
         return -1 * (1000 + (day - 1) * 100);
+    }
+
+    public int calculateWeekdayDiscount(HashMap<String, Integer> map) {
+        long dessertCount = map.keySet().stream()
+                .filter(name -> Menu.getDessertList().contains(name))
+                .mapToInt(map::get)
+                .sum();
+        return (int) (-1 * (dessertCount * 2023));
     }
 }
