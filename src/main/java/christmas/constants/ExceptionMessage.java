0dce6d3069ecdb05b7c7ccbb65616f1af4435f23
wda067 <mg6359@naver.com>
2023-11-13 13:33:46 +0900

    feat: 예외 메시지 enum 생성

---
+++ src/main/java/christmas/constants/ExceptionMessage.java
@@ -0,0 +1,17 @@
+package christmas.constants;
+
+public enum ExceptionMessage {
+    EXCEPTION_PREFIX("[ERROR] "),
+    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
