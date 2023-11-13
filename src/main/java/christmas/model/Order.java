82e466b62da418181df6129ba476938613b8cb7e
wda067 <mg6359@naver.com>
2023-11-13 11:35:11 +0900

    refactor: Order record 클래스로 수정

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -2,22 +2,5 @@ package christmas.model;
 
 import java.util.HashMap;
 
-public class Order {
-    private final HashMap<String, Integer> map;
-
-    public Order(HashMap<String, Integer> map) {
-        this.map = map;
-    }
-
-    public int getCount() {
-        int count = 0;
-        for (Integer value : map.values()) {
-            count += value;
-        }
-        return count;
-    }
-
-    public HashMap<String, Integer> getMap() {
-        return map;
-    }
+public record Order(HashMap<String, Integer> map) {
 }
