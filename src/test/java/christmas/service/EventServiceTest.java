d9325799873578e0bf981c785e3852ae819d93f7
wda067 <mg6359@naver.com>
2023-11-13 22:12:09 +0900

    test: 평일/주말 구분하여 테스트

--- src/test/java/christmas/service/EventServiceTest.java
+++ src/test/java/christmas/service/EventServiceTest.java
@@ -1,9 +1,10 @@
 package christmas.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
+import christmas.constants.Event;
+import christmas.model.Order;
+import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,14 @@ import org.junit.jupiter.api.Test;
 class EventServiceTest {
 
     EventService eventService;
+    OrderService orderService;
+    Order order;
 
     @BeforeEach
     void seuUp() {
         eventService = new EventService();
+        orderService = new OrderService();
+        order = new Order(orderService.convertStringToCollection("티본스테이크-1,바비큐립-1,초코케이크-1,제로콜라-1"));
     }
 
     @DisplayName("크리스마스 디데이 할인 테스트")
@@ -27,6 +32,19 @@ class EventServiceTest {
                 .isEqualTo(-1200);
         assertThat(eventService.calculateXmasDiscount(day2))
                 .isEqualTo(0);
+    }
+
+    @DisplayName("평일/주말 구분하여 할인 테스트")
+    @Test
+    void calculateWeekdayDiscount() {
+        int weekday = 3;
+        int weekend = 8;
+        HashMap<String, Integer> benefitDetails = new HashMap<>();
+        eventService.determineDayDiscount(weekday, benefitDetails, order.map());
+        eventService.determineDayDiscount(weekend, benefitDetails, order.map());
+
+        assertThat(benefitDetails).containsEntry("평일 할인: ", -2023);
+        assertThat(benefitDetails).containsEntry("주말 할인: ", -4046);
     }
 
 }
