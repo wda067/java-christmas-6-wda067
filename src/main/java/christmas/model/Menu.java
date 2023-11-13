f36437c643059cedf5d820023ea32f57a07707df
wda067 <mg6359@naver.com>
2023-11-14 00:42:37 +0900

    refactor: 영어를 한글로 수정

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -5,18 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Menu {
-    BUTTON_MUSHROOM_SOUP("appetizer", "양송이수프", 6000),
-    TAPAS("appetizer", "타파스", 5500),
-    CAESAR_SALAD("appetizer", "시저샐러드", 8000),
-    T_BONE_STEAK("main", "티본스테이크", 55000),
-    BBQ_RIBS("main", "바비큐립", 54000),
-    SEAFOOD_PASTA("main", "해산물파스타", 35000),
-    CHRISTMAS_PASTA("main", "크리스마스파스타", 25000),
-    CHOCO_CAKE("dessert", "초코케이크", 15000),
-    ICE_CREAM("dessert", "아이스크림", 5000),
-    ZERO_COLA("beverage", "제로콜라", 3000),
-    RED_WINE("beverage", "레드와인", 60000),
-    CHAMPAGNE("beverage", "샴페인", 25000);
+    BUTTON_MUSHROOM_SOUP("애피타이저", "양송이수프", 6000),
+    TAPAS("애피타이저", "타파스", 5500),
+    CAESAR_SALAD("애피타이저", "시저샐러드", 8000),
+    T_BONE_STEAK("메인", "티본스테이크", 55000),
+    BBQ_RIBS("메인", "바비큐립", 54000),
+    SEAFOOD_PASTA("메인", "해산물파스타", 35000),
+    CHRISTMAS_PASTA("메인", "크리스마스파스타", 25000),
+    CHOCO_CAKE("디저트", "초코케이크", 15000),
+    ICE_CREAM("디저트", "아이스크림", 5000),
+    ZERO_COLA("음료", "제로콜라", 3000),
+    RED_WINE("음료", "레드와인", 60000),
+    CHAMPAGNE("음료", "샴페인", 25000);
 
     private final String menu;
     private final String name;
@@ -36,21 +36,21 @@ public enum Menu {
 
     public static List<String> getDessertList() {
         return Arrays.stream(Menu.values())
-                .filter(menu -> menu.getMenu().equals("dessert"))
+                .filter(menu -> menu.getMenu().equals("디저트"))
                 .map(Menu::getName)
                 .collect(Collectors.toList());
     }
 
     public static List<String> getMainList() {
         return Arrays.stream(Menu.values())
-                .filter(menu -> menu.getMenu().equals("main"))
+                .filter(menu -> menu.getMenu().equals("메인"))
                 .map(Menu::getName)
                 .collect(Collectors.toList());
     }
 
     public static List<String> getBeverageList() {
         return Arrays.stream(Menu.values())
-                .filter(menu -> menu.getMenu().equals("beverage"))
+                .filter(menu -> menu.getMenu().equals("음료"))
                 .map(Menu::getName)
                 .collect(Collectors.toList());
     }
