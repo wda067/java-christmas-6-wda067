ab23151938928fbe49ca9ac184468a60df4ad762
wda067 <mg6359@naver.com>
2023-11-13 14:14:18 +0900

    feat: 쉼표 정규표현식 추가

--- src/main/java/christmas/constants/Regex.java
+++ src/main/java/christmas/constants/Regex.java
@@ -2,7 +2,8 @@ package christmas.constants;
 
 public enum Regex {
     NON_DIGIT_REGEX("\\D+"),
-    MENU_AND_COUNT_REGEX("([가-힣]+)-(\\d+)");
+    MENU_AND_COUNT_REGEX("([가-힣]+)-(\\d+)"),
+    COMMA_REGEX(",");
     private final String regex;
 
     Regex(String regex) {
