69c23ebf8a2e427c232d528593f6c7612c0c65b1
wda067 <mg6359@naver.com>
2023-11-13 14:15:52 +0900

    refactor: 출력과 입력 메서드 구분

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,18 +2,29 @@ package christmas.view;
 
 import static christmas.constants.Message.INPUT_MENU_AND_COUNT;
 import static christmas.constants.Message.INPUT_VISIT_DATE_MESSAGE;
+import static christmas.constants.Message.OUTPUT_WELCOME_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public String inputVisitDate() {
+    public void printWelcomeMessage() {
+        System.out.println(OUTPUT_WELCOME_MESSAGE.getMessage());
+    }
+
+    public void printInputVisitDateMessage() {
         System.out.println(INPUT_VISIT_DATE_MESSAGE.getMessage());
+    }
+
+    public void printInputMenuAndCountMessage() {
+        System.out.println(INPUT_MENU_AND_COUNT.getMessage());
+    }
+
+    public String inputVisitDate() {
         return Console.readLine();
     }
 
     public String inputMenuAndCount() {
-        System.out.println(INPUT_MENU_AND_COUNT.getMessage());
         return Console.readLine();
     }
 }
