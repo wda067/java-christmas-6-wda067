38010c21648c9582dab97f3f7ea59fd2c32a8f2f
wda067 <mg6359@naver.com>
2023-11-14 01:26:59 +0900

    feat: 메뉴판 출력 기능

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,10 +1,17 @@
 package christmas.view;
 
+import static christmas.constants.Message.INPUT_EVENT_NOTICE_MESSAGE;
 import static christmas.constants.Message.INPUT_MENU_AND_COUNT;
+import static christmas.constants.Message.INPUT_MENU_TABLE_MESSAGE;
 import static christmas.constants.Message.INPUT_VISIT_DATE_MESSAGE;
 import static christmas.constants.Message.OUTPUT_WELCOME_MESSAGE;
+import static christmas.util.NumberFormatter.formatNumber;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.model.Menu;
+import christmas.util.NumberFormatter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class InputView {
 
@@ -16,6 +23,21 @@ public class InputView {
         System.out.println(INPUT_VISIT_DATE_MESSAGE.getMessage());
     }
 
+    public void printMenuTable() {
+        System.out.println(INPUT_MENU_TABLE_MESSAGE.getMessage());
+        Arrays.stream(Menu.values())
+                .map(Menu::getMenu)
+                .distinct()
+                .forEach(menuType -> {
+                    System.out.println("<" + menuType + ">");
+                    String menuItems = Arrays.stream(Menu.values())
+                            .filter(menu -> menu.getMenu().equals(menuType))
+                            .map(menu -> menu.getName() + "(" + formatNumber(menu.getPrice()) + ")")
+                            .collect(Collectors.joining(", "));
+                    System.out.println(menuItems);
+                });
+    }
+    
     public void printInputMenuAndCountMessage() {
         System.out.println(INPUT_MENU_AND_COUNT.getMessage());
     }
