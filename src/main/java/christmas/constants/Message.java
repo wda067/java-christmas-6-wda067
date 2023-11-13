8ef51fa060680200595de85421b9c9f2250d8e51
wda067 <mg6359@naver.com>
2023-11-14 01:25:50 +0900

    feat: 주의사항 추가

--- src/main/java/christmas/constants/Message.java
+++ src/main/java/christmas/constants/Message.java
@@ -2,7 +2,15 @@ package christmas.constants;
 
 public enum Message {
     INPUT_VISIT_DATE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    INPUT_MENU_TABLE_MESSAGE("<메뉴판>"),
     INPUT_MENU_AND_COUNT("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    INPUT_EVENT_NOTICE_MESSAGE("""
+            <이벤트 주의 사항>
+            총주문 금액 10,000원 이상부터 이벤트가 적용됩니다.
+            음료만 주문 시, 주문할 수 없습니다.
+            메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.
+            (e.g. 시저샐러드-1, 티본스테이크-1, 크리스마스파스타-1, 제로콜라-3, 아이스크림-1의 총개수는 7개)
+            """),
     OUTPUT_WELCOME_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
     OUTPUT_OVERVIEW_MESSAGE("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n"),
     OUTPUT_ORDER_MENU_MESSAGE("<주문 메뉴>"),
