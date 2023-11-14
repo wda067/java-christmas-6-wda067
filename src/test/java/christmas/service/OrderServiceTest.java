9b3eb157f789236972bdf5914169fbf3fed284f3
wda067 <mg6359@naver.com>
2023-11-14 22:06:37 +0900

    style: 불필요 import 제거

--- src/test/java/christmas/service/OrderServiceTest.java
+++ src/test/java/christmas/service/OrderServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.model.Order;
-import java.util.HashMap;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
